ef0ee77cbd700dd26109e1c1a4eff1e2f569f3fd
Nicolas Parra <thndrdbeta@gmail.com>
2022-10-08 23:29:27 -0500

    Ejercicio-funciones

---
+++ ejercicio-funciones.js
@@ -0,0 +1,18 @@
+function funcionTrue() {
+    return true;
+}
+
+async function promesaTime() {
+    return setTimeout(() => console.log("Hola soy una promesa"), 5000)
+}
+
+promesaTime();
+
+function* idsPares() {
+    let id = 0
+    while(true) {
+        yield id += 2
+    }
+}
+
+idsPares();
